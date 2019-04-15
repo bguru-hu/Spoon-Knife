c5ef684944f978ae5ab1179e2773ca6b61baf09f
bguru-hu <49656108+bguru-hu@users.noreply.github.com>
2019-04-15 14:42:07 -0400

    Create HelloWorld.java
    test

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package h3;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Best friend: Joel");
+		System.out.println("Best friend: Anudeep");
+		System.out.println("Best friend: Kishan");
+	}
+
+}
